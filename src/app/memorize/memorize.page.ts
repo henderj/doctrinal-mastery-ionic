bdb2af2a53f7d7c3439ad32a68f027e3fe1d4d00
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-01-31 14:11:45 -0500

    added memorize page

---
+++ src/app/memorize/memorize.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memorize',
+  templateUrl: './memorize.page.html',
+  styleUrls: ['./memorize.page.scss'],
+})
+export class MemorizePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
