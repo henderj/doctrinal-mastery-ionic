857224f9d305d0aad3bff14f6832811fbb12d92b
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-01-31 17:30:51 -0500

    added multiple choice component

---
+++ src/app/memorize/multiple-choice/multiple-choice.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiple-choice',
+  templateUrl: './multiple-choice.component.html',
+  styleUrls: ['./multiple-choice.component.scss'],
+})
+export class MultipleChoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
