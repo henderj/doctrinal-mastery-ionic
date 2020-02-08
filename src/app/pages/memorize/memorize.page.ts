d4c4749c40463b3a5e0293d8a1c4022fbbba12d3
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-02-08 16:10:50 -0500

    added logic to memorize service

--- src/app/pages/memorize/memorize.page.ts
+++ src/app/pages/memorize/memorize.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ChallengePayload } from '../../interfaces/ChallengePayload';
 import { Book } from '../../interfaces/book';
 import { Item, ItemType } from '../../interfaces/item';
+import { MemorizeService } from 'src/app/services/memorize.service';
+import { MemorizeChallenges } from 'src/app/enums/memorize-challenges.enum';
 
 @Component({
   selector: 'app-memorize',
@@ -12,9 +14,7 @@ export class MemorizePage implements OnInit {
 
   useCardChallenge = false;
 
-  readonly MaxCurrentItems = 4;
-
-  currentBook: Book = new Book('test', [
+  book: Book = new Book('test', [
     new Item(0, ItemType.Doctrine, 'test 1: reference(Q)', 'test 1: doctrine(A)'),
     new Item(1, ItemType.Reference, 'test 1: doctrine(Q)', 'test 1: reference(A)'),
     new Item(2, ItemType.Doctrine, 'test 2: reference(Q)', 'test 2: doctrine(A)'),
@@ -24,160 +24,47 @@ export class MemorizePage implements OnInit {
     new Item(6, ItemType.Doctrine, 'test 4: reference(Q)', 'test 4: doctrine(A)'),
     new Item(7, ItemType.Reference, 'test 4: doctrine(Q)', 'test 4: reference(A)'),
   ]);
-  currentView = '';
-  currentViewCounter = 0;
 
-  maxScore = 10;
-  currentScore = 0;
-
-  currentItems: Item[] = [];
-  itemIndex = 0;
-
-  masteredItems: Item[] = [];
-
-  get book(): Book {
-    return this.currentBook;
-  }
+  currentItem: Item;
 
   get currentProperties(): object {
     return {
       book: this.book,
-      item: this.currentItems[this.itemIndex]
+      item: this.currentItem
     };
   }
 
-  get currentItem(): Item {
-    return this.currentItems[this.itemIndex];
-  }
 
   get progressPercent(): number {
-    let percent: number = Math.abs((this.currentScore / this.maxScore) * 100);
-    if (percent > 100) { percent = 100; }
-    if (percent < 0) { percent = 0; }
-
-    return percent;
+    return this.memorizeService.progressPercent;
   }
 
-  created() {
-    this.maxScore = this.calcMaxScore();
-    this.setNextItem();
-    this.setView();
-  }
 
-  private calcMaxScore(): number {
-    if (this.currentBook.pointsLeft <= 0) { return 10; }
-
-    if (this.currentBook.pointsLeft > 10) { return 10; }
-
-    return this.currentBook.pointsLeft;
-  }
-
-  private setNextItem(): void {
-    if (this.currentItems.length >= this.MaxCurrentItems) {
-      this.itemIndex = Math.floor(Math.random() * this.currentItems.length);
-      return;
-    }
-
-    let nextItem: Item;
-
-    if (this.book.itemsNotSeen.length + this.book.itemsLearning.length > 0) {
-      nextItem = this.book.chooseRandomSingle(
-        this.book.itemsNotSeen.concat(this.book.itemsLearning)
-      );
-
-      const nextItemIndex = this.currentItems.indexOf(nextItem);
-      if (nextItemIndex === -1) {
-        this.currentItems.push(nextItem);
-        this.itemIndex = this.currentItems.length - 1;
-        return;
-      }
-
-      this.itemIndex = nextItemIndex;
-      return;
-    }
-
-    nextItem = this.book.chooseRandomSingle(this.book.itemsMastered);
-    this.currentItems.push(nextItem);
-    this.itemIndex = this.currentItems.length - 1;
-
-  }
-
-  private setView(): void {
-    this.currentViewCounter++;
-    if (this.currentItem.score >= 2) {
-      this.currentView = 'CardChallenge';
+  private updateView(view: MemorizeChallenges): void {
+    if (view === MemorizeChallenges.Card) {
       this.useCardChallenge = true;
       return;
     }
-    this.currentView = 'MultipleChoiceChallenge';
+
     this.useCardChallenge = false;
   }
 
-  private finishSession(): void {
-    // this.$store.commit('updateMasteredItems', {
-    //   masteredItems: this.masteredItems
-    // });
-    // this.$router.push({
-    //   name: 'finished'
-    // });
-  }
 
-  private removeItemFromArray(item: Item, array: Item[]): void {
-    const index = array.indexOf(item);
-    if (index !== -1) { array.splice(index, 1); }
-  }
-
-  // onFinishedQuestion(correct: boolean, scoreDelta: number): void {
-  //   if (correct) {
-  //     this.currentItem.incrementScore(scoreDelta);
-  //     // this.$store.dispatch('saveItem', this.currentItem);
-  //     if (this.currentItem.mastered) {
-  //       this.masteredItems.push(this.currentItem);
-  //       this.removeItemFromArray(this.currentItem, this.currentItems);
-  //     }
-  //     this.currentScore += scoreDelta;
-  //   } else {
-  //     this.currentItem.decrementScore(Math.abs(scoreDelta));
-  //     // this.$store.dispatch('saveItem', this.currentItem);
-  //   }
-
-  //   if (this.currentScore >= this.maxScore) {
-  //     this.finishSession();
-  //   } else {
-  //     this.setNextItem();
-  //     this.setView();
-  //   }
-  // }
-
-  constructor() { }
+  constructor(public memorizeService: MemorizeService) { }
 
   ngOnInit() {
-    this.maxScore = this.calcMaxScore();
-    this.setNextItem();
-    this.setView();
+    this.memorizeService.startMemorizeSession(this.book, this.onNextItemReady);
+  }
+
+  onNextItemReady(item: Item, view: MemorizeChallenges) {
+    this.updateView(view);
+    this.currentItem = item;
   }
 
   onFinished(payload: ChallengePayload) {
     console.log('finished: ', payload);
-    if (payload.correct) {
-      this.currentItem.incrementScore(payload.scoreDelta);
-      // this.$store.dispatch('saveItem', this.currentItem);
-      if (this.currentItem.mastered) {
-        this.masteredItems.push(this.currentItem);
-        this.removeItemFromArray(this.currentItem, this.currentItems);
-      }
-      this.currentScore += payload.scoreDelta;
-    } else {
-      this.currentItem.decrementScore(Math.abs(payload.scoreDelta));
-      // this.$store.dispatch('saveItem', this.currentItem);
-    }
 
-    if (this.currentScore >= this.maxScore) {
-      this.finishSession();
-    } else {
-      this.setNextItem();
-      this.setView();
-    }
+    this.memorizeService.onFinished(payload);
   }
 
 }
--- src/app/services/memorize.service.ts
+++ src/app/services/memorize.service.ts
@@ -1,12 +1,169 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Item } from '../interfaces/item';
+import { Item, ItemType } from '../interfaces/item';
+import { removeItemFromArray } from '../utils';
+import { Book } from '../interfaces/book';
+import { ChallengePayload } from '../interfaces/ChallengePayload';
+import { MemorizeChallenges } from '../enums/memorize-challenges.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemorizeService {
-  public currentItem: Observable<Item>;
+
+  get book(): Book {
+    return this.currentBook;
+  }
+
+  get currentProperties(): object {
+    return {
+      book: this.book,
+      item: this.currentItems[this.currentItemIndex]
+    };
+  }
+
+  get currentItem(): Item {
+    return this.currentItems[this.currentItemIndex];
+  }
+
+  get progressPercent(): number {
+    let percent: number = Math.abs((this.currentScore / this.maxScore) * 100);
+    if (percent > 100) { percent = 100; }
+    if (percent < 0) { percent = 0; }
+
+    return percent;
+  }
 
   constructor() { }
+  readonly MaxCurrentItems = 4;
+
+  currentBook: Book = new Book('test', [
+    new Item(900, ItemType.Doctrine, 'test 1: reference(Q)', 'test 1: doctrine(A)'),
+    new Item(901, ItemType.Reference, 'test 1: doctrine(Q)', 'test 1: reference(A)'),
+    new Item(902, ItemType.Doctrine, 'test 2: reference(Q)', 'test 2: doctrine(A)'),
+    new Item(903, ItemType.Reference, 'test 2: doctrine(Q)', 'test 2: reference(A)'),
+    new Item(904, ItemType.Doctrine, 'test 3: reference(Q)', 'test 3: doctrine(A)'),
+    new Item(905, ItemType.Reference, 'test 3: doctrine(Q)', 'test 3: reference(A)'),
+    new Item(906, ItemType.Doctrine, 'test 4: reference(Q)', 'test 4: doctrine(A)'),
+    new Item(907, ItemType.Reference, 'test 4: doctrine(Q)', 'test 4: reference(A)'),
+  ]);
+
+
+  maxScore = 10;
+  currentScore = 0;
+
+  currentItems: Item[] = [];
+  currentItemIndex = 0;
+
+  masteredItems: Item[] = [];
+
+  nextItemCallback: (item: Item, view: MemorizeChallenges) => void;
+
+
+  private static calcMaxScore(book: Book): number {
+    if (book.pointsLeft <= 0) { return 10; }
+
+    if (book.pointsLeft > 10) { return 10; }
+
+    return book.pointsLeft;
+  }
+
+  public startMemorizeSession(book: Book, onNextItemReady?: (item: Item, view: MemorizeChallenges) => void): void {
+    this.currentBook = book;
+    this.maxScore = MemorizeService.calcMaxScore(book);
+    this.nextItemCallback = onNextItemReady;
+
+    this.continueSession();
+  }
+
+  private nextView(item: Item): MemorizeChallenges {
+    if (item.score >= Item.MaxScore - 1) {
+      return MemorizeChallenges.Card;
+    }
+
+    return MemorizeChallenges.MultipleChoice;
+  }
+
+  private getNextItemIndex(): number {
+    const nextItem = this.getNextItem();
+    const nextItemIndex = this.currentItems.indexOf(nextItem);
+
+    if (nextItemIndex === -1) {
+      this.currentItems.push(nextItem);
+      return this.currentItems.length - 1;
+    }
+
+    return nextItemIndex;
+  }
+
+  private getNextItem(): Item {
+    if (this.currentItems.length >= this.MaxCurrentItems) {
+      return this.currentItems[Math.floor(Math.random() * this.currentItems.length)];
+    }
+
+    let nextItem: Item;
+
+    if (this.book.itemsNotSeen.length + this.book.itemsLearning.length > 0) {
+      nextItem = this.book.chooseRandomSingle(
+        this.book.itemsNotSeen.concat(this.book.itemsLearning)
+      );
+
+      return nextItem;
+
+      // const nextItemIndex = this.currentItems.indexOf(nextItem);
+      // if (nextItemIndex === -1) {
+      //   this.currentItems.push(nextItem);
+      //   this.currentItemIndex = this.currentItems.length - 1;
+      //   return;
+      // }
+
+      // this.currentItemIndex = nextItemIndex;
+      // return;
+    }
+
+    nextItem = this.book.chooseRandomSingle(this.book.itemsMastered);
+    return nextItem;
+    // this.currentItems.push(nextItem);
+    // this.currentItemIndex = this.currentItems.length - 1;
+
+  }
+
+  private continueSession(): void {
+    this.currentItemIndex = this.getNextItemIndex();
+    const nextView = this.nextView(this.currentItem);
+
+    this.nextItemCallback(this.currentItem, nextView);
+  }
+
+  private finishSession(): void {
+    // this.$store.commit('updateMasteredItems', {
+    //   masteredItems: this.masteredItems
+    // });
+    // this.$router.push({
+    //   name: 'finished'
+    // });
+  }
+
+  onFinished(payload: ChallengePayload) {
+    console.log('finished: ', payload);
+
+    if (payload.correct) {
+      this.currentItem.incrementScore(payload.scoreDelta);
+      // this.$store.dispatch('saveItem', this.currentItem);
+      if (this.currentItem.mastered) {
+        this.masteredItems.push(this.currentItem);
+        removeItemFromArray(this.currentItem, this.currentItems);
+      }
+      this.currentScore += payload.scoreDelta;
+    } else {
+      this.currentItem.decrementScore(Math.abs(payload.scoreDelta));
+      // this.$store.dispatch('saveItem', this.currentItem);
+    }
+
+    if (this.currentScore >= this.maxScore) {
+      this.finishSession();
+    } else {
+      this.continueSession();
+    }
+  }
 }
