160659acb964ca2eb5981372183592f65bcf8786
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-03-17 15:03:11 -0400

    added firebaseConfig file

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDb07B1nv0May__cQB7eVoHTTwSRZa1oQ4',
+    authDomain: 'doctrinal-mastery-ebe13.firebaseapp.com',
+    databaseURL: 'https://doctrinal-mastery-ebe13.firebaseio.com',
+    projectId: 'doctrinal-mastery-ebe13',
+    storageBucket: 'doctrinal-mastery-ebe13.appspot.com',
+    messagingSenderId: '66956608965',
+    appId: '1:66956608965:web:60ff2741510fd3f6ebb7ac'
+};
